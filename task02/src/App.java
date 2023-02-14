2af106539344f7b0e23c3200e4c68577cd947af3
Caleb <5322420+calebnjw@users.noreply.github.com>
2023-02-14 12:53:11 +0800

    remove all punctuation, including apostrophes

--- task02/src/App.java
+++ task02/src/App.java
@@ -3,6 +3,7 @@ package task02;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -33,31 +34,61 @@ public class App {
       return;
     }
 
-    // create list of all files in the folder
-    // this creates a list with file names only
-    // List<String> fileListString = new LinkedList<String>();
-    // fileListString = Arrays.asList(path.list());
-    // System.out.println(fileListString);
-
     // this creates a list with file paths. we want this.
     List<File> fileList = new LinkedList<File>();
     fileList = Arrays.asList(path.listFiles());
     System.out.println(fileList);
 
-    // read file one by one using buffered reader?
-    String line;
-    InputStream is = new FileInputStream(fileList.get(0));
+    String combinedText = "";
+    for (File f : fileList) {
+      String oneLine = fileToString(f);
+      combinedText += oneLine + " ";
+    }
+
+    System.out.println(combinedText);
+
+    // look through each document
+    // remove all punctuation (but things like I'll and we'll should be left
+    // intact?)
+    // collect all words from all files into an object
+    //// might wanna make a new class for words?
+    //// words will store a list of words that follow
+    //// and count how many of those words occur
+    //// should also be able to sum up the total number of next words for
+    // probability
+    //
+
+  }
+
+  public static String fileToString(File f) throws FileNotFoundException, IOException {
+    String fullText = ""; // to store full text as one string and return that
+    String nextLine;
+
+    // read lines from file one by one
+    InputStream is = new FileInputStream(f);
     InputStreamReader isr = new InputStreamReader(is);
     BufferedReader br = new BufferedReader(isr);
 
-    while ((line = br.readLine()) != null) {
-      System.out.println(line);
+    // dump all text into fullText
+    while ((nextLine = br.readLine()) != null) {
+      fullText += nextLine + " ";
     }
 
     br.close();
     isr.close();
     is.close();
 
-    // strip punctuation marks
+    // regular expressions to remove punctuation but keep apostrophe
+    // we'll != well
+    // I'll != ill
+    String punctuationRegex = "\\p{Punct}";
+    String spacesRegex = " +";
+
+    // strip punctuation marks and extra spaces
+    fullText = fullText.trim()
+        .replaceAll(punctuationRegex, "")
+        .replaceAll(spacesRegex, " ");
+
+    return fullText;
   }
 }
